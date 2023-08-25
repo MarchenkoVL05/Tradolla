1b68bb03f17355c59ad2f32fc2c5882c1f8dba1a
HP 6 <Shamoointhedark@gmail.com>
2023-08-25 15:35:19 +0500

    add request to favorites logic

--- resources/js/Components/RequestFavorite.vue
+++ resources/js/Components/RequestFavorite.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="flex items-center gap-2 text-gray-400">
-        <button>
-            <img class="h-4" src="/images/star-favorite.svg" alt="" />
+        <button @click="addRequestToFavorite">
+            <img
+                class="h-4"
+                :src="
+                    !requestAdded
+                        ? '/images/star-favorite.svg'
+                        : '/images/star-favorite--fill.svg'
+                "
+                alt=""
+            />
         </button>
         <div class="text-sm">Request (№ <span>000023</span>)</div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            requestAdded: false,
+        };
+    },
+
+    methods: {
+        addRequestToFavorite() {
+            this.requestAdded = !this.requestAdded;
+        },
+    },
+};
 </script>
