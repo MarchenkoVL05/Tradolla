2097bc0e86bbafae052c2546844dc7f51f98f83d
HP 6 <Shamoointhedark@gmail.com>
2023-09-04 13:43:59 +0500

    company-info-table component

--- resources/js/Components/Company/CompanyInfoTable.vue
+++ resources/js/Components/Company/CompanyInfoTable.vue
@@ -1,5 +1,114 @@
 <template>
-    <div></div>
+    <div class="flex flex-col text-sm sm:text-base">
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Headquarters</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >Taiwan, Taipei</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Company name</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >Chunghwa Telecom Co., Ltd.</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Company Registration Number</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >96979976</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Taxpayer Identification Number</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >01123456323</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >DUNS-number</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >31-41-634</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Year of foundation</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100">1996</span>
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Full-time staff</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100">1500</span>
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Annual income (USD)</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >$40 000 000</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Legal form</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >corporation</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Type of company</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >manufacturer</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Scope of activity</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >manufacturing</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Bank name</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >Taibai Bank</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >BIC code and/or SWIFT/IBAN</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >090293023012</span
+            >
+        </div>
+        <div class="grid grid-cols-2">
+            <span class="p-1 border-b bg-slate-100 text-gray-500"
+                >Account number</span
+            >
+            <span class="p-1 font-medium border-b border-gray-100"
+                >32032-323-235-23</span
+            >
+        </div>
+    </div>
 </template>
 
 <script>
--- resources/js/Pages/OrderDetails.vue
+++ resources/js/Pages/OrderDetails.vue
@@ -14,148 +14,7 @@
                                 >Chunghwa Telecom Ltd. (view profile)</a
                             >
                         </div>
-                        <div class="flex flex-col text-sm sm:text-base">
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Headquarters</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >Taiwan, Taipei</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Company name</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >Chunghwa Telecom Co., Ltd.</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Company Registration Number</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >96979976</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Taxpayer Identification Number</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >01123456323</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >DUNS-number</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >31-41-634</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Year of foundation</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >1996</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Full-time staff</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >1500</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Annual income (USD)</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >$40 000 000</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Legal form</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >corporation</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Type of company</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >manufacturer</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Scope of activity</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >manufacturing</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Bank name</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >Taibai Bank</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >BIC code and/or SWIFT/IBAN</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >090293023012</span
-                                >
-                            </div>
-                            <div class="grid grid-cols-2">
-                                <span
-                                    class="p-1 border-b bg-slate-100 text-gray-500"
-                                    >Account number</span
-                                >
-                                <span
-                                    class="p-1 font-medium border-b border-gray-100"
-                                    >32032-323-235-23</span
-                                >
-                            </div>
-                        </div>
+                        <company-info-table></company-info-table>
                     </div>
                 </template>
             </form-box>
@@ -192,6 +51,7 @@ import { Head as PageHead } from "@inertiajs/vue3";
 import BaseLayout from "../Layouts/BaseLayout.vue";
 import FormBox from "../Components/Forms/FormBox.vue";
 import ProcurementDetailsTable from "../Components/Procurement/ProcurementDetailsTable.vue";
+import CompanyInfoTable from "../Components/Company/CompanyInfoTable.vue";
 
 export default {
     components: {
@@ -199,6 +59,7 @@ export default {
         BaseLayout,
         FormBox,
         ProcurementDetailsTable,
+        CompanyInfoTable,
     },
 };
 </script>
