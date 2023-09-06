3a3ee154966311b468409de553991c637ebe63b4
HP 6 <Shamoointhedark@gmail.com>
2023-09-06 17:31:47 +0500

    auth-header component

---
+++ resources/js/Components/AuthHeader.vue
@@ -0,0 +1,66 @@
+<template>
+    <header
+        class="relative w-full h-12 flex items-center justify-between px-3 bg-white shadow"
+        :class="{ 'flex-col': opened, 'h-auto': opened, 'py-2': opened }"
+    >
+        <the-logo></the-logo>
+        <div class="hidden justify-center items-center gap-14 md:flex">
+            <the-header-language-select></the-header-language-select>
+            <the-header-menu></the-header-menu>
+            <div class="flex items-center justify-center gap-6">
+                <Link
+                    class="text-gray-800 transition-all hover:text-orange-500"
+                    href="/login"
+                >
+                    Sign In
+                </Link>
+                <Link href="/register">
+                    <button-orange class="px-4 py-2">Sign Up</button-orange>
+                </Link>
+            </div>
+        </div>
+        <button
+            @click="toggleHeader"
+            class="absolute right-3 w-7 h-7 flex items-center justify-center md:hidden"
+        >
+            <img
+                :src="
+                    opened
+                        ? '/images/burger-close.svg'
+                        : '/images/burger-icon.svg'
+                "
+                alt=""
+            />
+        </button>
+    </header>
+</template>
+
+<script>
+import { Link } from "@inertiajs/vue3";
+import TheLogo from "./TheLogo.vue";
+import TheHeaderLanguageSelect from "./Header/TheHeaderLanguageSelect.vue";
+import TheHeaderMenu from "./Header/TheHeaderMenu.vue";
+import ButtonOrange from "./Buttons/ButtonOrange.vue";
+
+export default {
+    components: {
+        Link,
+        TheLogo,
+        TheHeaderLanguageSelect,
+        TheHeaderMenu,
+        ButtonOrange,
+    },
+
+    data() {
+        return {
+            opened: false,
+        };
+    },
+
+    methods: {
+        toggleHeader() {
+            this.opened = !this.opened;
+        },
+    },
+};
+</script>
--- resources/js/Layouts/AuthLayout.vue
+++ resources/js/Layouts/AuthLayout.vue
@@ -1,16 +1,16 @@
 <template>
-    <the-header></the-header>
+    <auth-header></auth-header>
     <main class="h-full min-h-full md:bg-gray-100 md:h-auto md:pt-32 md:pb-32">
         <slot></slot>
     </main>
 </template>
 
 <script>
-import TheHeader from "../components/header/TheHeader.vue";
+import AuthHeader from "../Components/AuthHeader.vue";
 
 export default {
     components: {
-        TheHeader,
+        AuthHeader,
     },
 };
 </script>
